0b1e84ab1e95b1214676d2078627aef2688ed60a
murilo ferreira <ferreira.murilosilva@outlook.com>
2022-10-16 23:19:17 -0300

    modal-alterado

--- src/js/script.js
+++ src/js/script.js
@@ -1,25 +1,18 @@
-// Get the modal
-var modal = document.getElementById("myModal");
+window.onload=function(){
+    const modal = document.querySelector(".modal");
+    const trigger = document.querySelector(".btn");
+    const closeButton = document.querySelector(".close");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+    function toggleModal() {
+        modal.classList.toggle("show-modal");
+    }
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    function windowOnClick(event) {
+        if (event.target === modal) {
+            toggleModal();
+        }
+    }
+    trigger.addEventListener("click", toggleModal);
+    closeButton.addEventListener("click", toggleModal);
+    window.addEventListener("click", windowOnClick);
 }
