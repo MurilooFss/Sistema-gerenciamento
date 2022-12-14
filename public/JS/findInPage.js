2c63d440c5df8a7dc75afb566a6ce550ecbbd863
MurilooFss <ferreira.murilosilva@outlook.com>
2022-12-14 18:02:42 -0300

    add pesquisa na page historico

--- public/JS/findInPage.js
+++ public/JS/findInPage.js
@@ -1,4 +1,18 @@
 
+function find2() {
+    let input = document.getElementsByClassName('search2')[0].value
+    input = input.toLowerCase();
+    let x = document.getElementsByClassName('row');
+
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = "none";
+        }
+        else {
+            x[i].style.display = "flex";
+        }
+    }
+}
 function find() {
     let input = document.getElementById('search').value
     input = input.toLowerCase();
