97d43796c1ad784143d4df9400ceeba4056da066
MurilooFss <ferreira.murilosilva@outlook.com>
2023-01-19 21:58:47 -0300

    alterado visual nos preços

--- public/JS/estacionamentoF.js
+++ public/JS/estacionamentoF.js
@@ -6,9 +6,12 @@ document.querySelectorAll('#psera').forEach(function (b) {
         })
         document.querySelectorAll('button').forEach(i => {
             i.disabled = false
+            i.style.display = 'inline-block'
 
         }
+
         )
+        psera.style.display = 'none'
 
     })
 })
@@ -19,3 +22,18 @@ document.querySelectorAll('#serr2as').forEach(function (b) {
         window.location.reload()
     })
 })
+
+var coll = document.getElementsByClassName("item");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
